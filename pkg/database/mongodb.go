b016371cdf24eed62a7b694e4f31a8129cfcbc42
Denis Mukonin <dezmukonin16@gmail.com>
2026-03-08 22:06:23 +0300

    feat: init mongoDB

---
+++ pkg/database/mongodb.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"fmt",
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func InitMongoDB(mongoURI string) (*mongo.Client, *mongo.Database, error) {
+	// Контекст с timeout
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Подключиться
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		fmt.Fatal("Failed to connect to MongoDB:", err)
+		return nil, nil, err
+	}
+
+	// Проверить подключение
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Fatal("Failed to ping MongoDB:", err)
+		return nil, nil, err
+	}
+
+	fmt.Println("Connected to MongoDB")
+
+	db := client.Database("analytics")
+
+	// Создать индексы для оптимизации поиска
+	createIndexes(db)
+
+	return client, db, nil
+}
